f991a6edac9288a19eacaa8174727c391e23750c
mahesh24-s <maheshsonnari1234@gmail.com>
2025-11-17 19:34:19 +0530

    add default path to check backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,6 +20,10 @@ app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
 
+app.get("/", (_req, res) => {
+  res.json({ status: "ok,app backend is running successfully" });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", authenticate, taskRoutes);
 
